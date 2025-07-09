a79fe15524bfa514049f6af40c9b4f9fc84c16da
Daniel Salazar <dsnile.salazr@gmail.com>
2025-07-08 22:31:57 -0600

    Add provider and router

---
+++ internal/router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/dsniels/storage-service/internal/app"
+	"github.com/dsniels/storage-service/internal/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func InitRoutes(app *app.App) *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Exception)
+
+	router.Group(func(r chi.Router) {
+		r.Post("/UploadFile", app.Controller.HandleUploadFile)
+	})
+
+	return router
+}
