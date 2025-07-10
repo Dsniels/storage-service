2771198c9c84744f829b96a98fc299fa7f1acb30
Daniel Salazar <dsnile.salazr@gmail.com>
2025-07-10 00:34:45 -0600

    implement ReaderSeek interface to get a stream from a video

--- internal/storage/reader.go
+++ internal/storage/reader.go
@@ -4,40 +4,71 @@ import (
 	"context"
 	"io"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
 )
 
 type BlobReader struct {
-	Ctx       context.Context
-	Pos       int64
-	Size      int64
-	Reader    io.ReadCloser
-	container string
-	fileName  string
+	Ctx         context.Context
+	Pos         int64
+	Size        int64
+	buffer      []byte
+	bufferStart int64
 	*blob.Client
 }
 
 func (b *BlobReader) Read(p []byte) (int, error) {
+	if b.Pos >= b.Size {
+		return 0, io.EOF
+	}
+
+	if b.Pos >= b.bufferStart && b.Pos < b.bufferStart+int64(len(b.buffer)) {
+		start := b.Pos - b.bufferStart
+		n := copy(p, b.buffer[start:])
+		b.Pos += int64(n)
+		return n, nil
+	}
+
+	chunkSize := int64(2 * 1024 * 1024)
+	if remaining := b.Size - b.Pos; remaining < chunkSize {
+		chunkSize = remaining
+	}
+
+	res, err := b.Client.DownloadStream(b.Ctx, &blob.DownloadStreamOptions{
+		Range: azblob.HTTPRange{Count: chunkSize, Offset: b.Pos},
+	})
+	if err != nil {
+		return 0, err
+	}
+	defer res.Body.Close()
+
+	b.buffer, err = io.ReadAll(res.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	b.bufferStart = b.Pos
+	n := copy(p, b.buffer)
+	b.Pos += int64(n)
 	return n, nil
 }
 
 func (b *BlobReader) Seek(offset int64, whence int) (int64, error) {
 
-	var newOffset int64
-
 	switch whence {
 	case io.SeekStart:
-		newOffset = offset
+		b.Pos = offset
 	case io.SeekEnd:
-		newOffset = b.Offset + offset
+		b.Pos = b.Size + offset
+	case io.SeekCurrent:
+		b.Pos += offset
 	}
 
-	b.Reader.Close()
-	b.Reader = nil
+	if b.Pos < 0 {
+		b.Pos = 0
 
-	b.Offset = newOffset
-	b.Pos = 0
+	}
 
-	return b.Offset, nil
+	return b.Pos, nil
 
 }
