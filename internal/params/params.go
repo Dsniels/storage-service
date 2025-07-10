1f580ce5fd74c125d2b5aa27be1130ba377a591c
Daniel Salazar <dsnile.salazr@gmail.com>
2025-07-10 00:33:45 -0600

    fucntions to get params from query and populate a struct, and Get id from url

---
+++ internal/params/params.go
@@ -0,0 +1,9 @@
+package params
+
+type FileList struct {
+	Prefix string
+	Container string
+	V string 
+	Foo string 
+	Baz string
+}
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,7 +2,12 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"reflect"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type Response map[string]any
@@ -18,4 +23,39 @@ func WriteResponse(w http.ResponseWriter, status int, data any) error {
 	w.WriteHeader(status)
 	w.Write(json)
 	return nil
+}
+func GetIdFromUrl(r *http.Request) (string, error) {
+
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		return "", fmt.Errorf("must provide an id")
+	}
+	return id, nil
+}
+
+func GetQueryFromUrl(r *http.Request) (string, error) {
+
+	return "", nil
+}
+
+func GetParamsFromUrl(r *http.Request, s interface{}) error {
+	val := reflect.ValueOf(s)
+	log.Println(val)
+	if val.Kind() != reflect.Pointer && val.Elem().Kind() != reflect.Struct {
+		return fmt.Errorf("must pass a pointer to an struct")
+	}
+
+	el := val.Elem()
+	t := el.Type()
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		val := r.URL.Query().Get(field.Name)
+		if el.Field(i).CanSet() {
+			el.Field(i).SetString(val)
+		}
+	}
+
+	return nil
+
 }
