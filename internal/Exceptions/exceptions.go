e4d8bee7f746f571cc280f19c83ab2d78e94767b
Daniel Salazar <dsnile.salazr@gmail.com>
2025-07-11 22:26:59 -0600

    Refactor error handling

--- internal/Exceptions/exceptions.go
+++ internal/Exceptions/exceptions.go
@@ -3,36 +3,67 @@ package exceptions
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/dsniels/storage-service/internal/utils"
 )
 
-var ErrorNotFound = errors.New("404: Data Not Fount")
-var ErrorBadRequest = errors.New("400: Bad Request")
+var ErrorNotFound = errors.New("data not found")
+var ErrorInternal = errors.New("something went wrong")
+var ErrorBadRequest = errors.New("bad request")
 
 func HandleError(w http.ResponseWriter, err interface{}) {
 
 	str := fmt.Sprint(err)
+	log.Println(str)
 	arr := strings.Split(str, ":")
+	log.Println(arr)
 
 	key := arr[0]
-	message := strings.Trim(arr[1], " ")
+	val := arr[len(arr)-1]
+	message := strings.Trim(val, " ")
 
 	switch key {
 	case "400":
-		utils.WriteResponse(w, http.StatusBadRequest, message)
+		utils.WriteResponse(w, http.StatusBadRequest, &utils.Response{"message": message})
 	case "404":
-		utils.WriteResponse(w, http.StatusNotFound, message)
+		utils.WriteResponse(w, http.StatusNotFound, utils.Response{"message": message})
 	default:
-		utils.WriteResponse(w, http.StatusInternalServerError, message)
+		utils.WriteResponse(w, http.StatusInternalServerError, utils.Response{"message": message})
 	}
 
 }
 
 func ThrowException(statusCode int, message string) {
 	err := errors.New(message)
-	err = fmt.Errorf("%v: %w", statusCode, err)
+	log.Println(err)
+	err = fmt.Errorf("%v: %v", statusCode, err)
 	panic(err)
+}
+
+func ThrowNotFound(messages ...string) {
+	var err error = ErrorNotFound
+	if len(messages) > 0 {
+		err = errors.New(messages[0])
+	}
+	ThrowException(http.StatusNotFound, err.Error())
+
+}
+
+func ThrowBadRequest(messages ...string) {
+	var err error = ErrorBadRequest
+	if len(messages) > 0 {
+		err = errors.New(messages[0])
+	}
+	ThrowException(http.StatusBadRequest, err.Error())
+
+}
+func ThrowInternalServerError(message ...string) {
+	var err error = ErrorInternal
+	if len(message) > 0 {
+		err = errors.New(message[0])
+	}
+	ThrowException(http.StatusInternalServerError, err.Error())
 }
